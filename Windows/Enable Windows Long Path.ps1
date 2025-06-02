de0db7ad2ba10fb05436220b534fd7c03c5d4d3a
Aymen EL JAZIRI <63322207+aymenjaz@users.noreply.github.com>
2025-06-02 09:26:03 -0400

    Enable Windows Long Path.ps1

--- Windows/Enable Windows Long Path.ps1
+++ Windows/Enable Windows Long Path.ps1
@@ -1,3 +1,2 @@
-
-
+# Enable Long Path for Microsoft Windows 10 / 11
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
