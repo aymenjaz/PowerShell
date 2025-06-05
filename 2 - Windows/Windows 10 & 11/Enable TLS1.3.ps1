7f0b34e50caf58732dcd14ed750cf9b135cffeab
Aymen EL JAZIRI <63322207+aymenjaz@users.noreply.github.com>
2025-06-05 16:46:29 -0400

    Create Enable TLS1.3.ps1

---
+++ 2 - Windows/Windows 10 & 11/Enable TLS1.3.ps1
@@ -0,0 +1,39 @@
+
+# enable it using registry for the client:
+
+Set-Location HKLM:
+$RegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.3\Client"
+$TempPath = "HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols"
+if(Test-Path $RegPath)
+{
+    $Items = Get-Item $RegPath | Select-Object Property
+    if($Items.Property -contains "DisabledByDefault")
+    {
+        Set-ItemProperty -Path $RegPath -Name "DisabledByDefault" -Value 0
+    }
+    else 
+    {
+        New-ItemProperty -Path $RegPath -Name "DisabledByDefault" -PropertyType "DWORD"  -Value 0
+    }
+    if($Items.Property -contains "Enabled")
+    {
+        Set-ItemProperty -Path $RegPath -Name "Enabled" -Value 1
+    }
+    else 
+    {
+        New-ItemProperty -Path $RegPath -Name "Enabled" -PropertyType "DWORD"  -Value 1
+    }
+    
+}
+elseif(Test-Path $TempPath) 
+{
+    Set-Location $TempPath
+    New-Item -Name "TLS 1.3"
+    New-Item -Path ($TempPath+"\TLS 1.3") -Name "Client"
+    Set-Location $RegPath
+    New-ItemProperty -Path $RegPath -Name "DisabledByDefault" -PropertyType "DWORD"  -Value 0
+    New-ItemProperty -Path $RegPath -Name "Enabled" -PropertyType "DWORD"  -Value 1
+}
+
+
+
